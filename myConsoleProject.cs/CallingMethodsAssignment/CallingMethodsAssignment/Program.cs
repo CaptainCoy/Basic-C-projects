6462a5abf93ae5ed8e8669246f3c1412467b67d4
Alex Coy <88282738+CaptainCoy@users.noreply.github.com>
2022-03-08 13:38:36 -0700

    Add files via upload

---
+++ myConsoleProject.cs/CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        public int Num1 { get; set; }
+        //public int Num2 { get; set; }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number...");
+            var num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("And another...");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("What do you want to do with these? (+, -, x,");
+            //var doWhat = Console.ReadLine();
+            var doWhatObject = new doWhat();
+            var funResult = doWhatObject.Add(num1);
+            funResult = doWhatObject.Subtract(funResult);
+            funResult = doWhatObject.Divide(funResult);
+            Console.WriteLine("Your number + 5 - 1 / 4 is " + funResult);
+            Console.ReadLine();
+
+            
+        }        
+    }
+}
---
+++ myConsoleProject.cs/CallingMethodsAssignment/CallingMethodsAssignment/doWhat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    public class doWhat
+    {
+        public int Add(int num1)
+        {
+            return num1 + 5;
+        }
+
+        public int Subtract(int num1)
+        {
+            return num1 - 1;
+        }
+
+        public int Divide(int num1)
+        {
+            return num1 / 4;
+        }
+
+
+        //public int GetAdd(int num1, int num2)
+        //{
+        //    return num1 + num2;
+
+        //} 
+
+        //public int GetSubtract(int num1, int num2)
+        //{
+        //    return num1 - num2;
+        //}
+
+        //public int GetMultiply(int num1, int num2)
+        //{
+        //    return num1 * num2;
+        //}
+    }
+}
