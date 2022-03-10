2369b6e322c05bd9bfea4b3e27d38c75be02ef20
Alex Coy <88282738+CaptainCoy@users.noreply.github.com>
2022-03-10 14:00:00 -0700

    Add files via upload

---
+++ myConsoleProject.cs/mainMethodAssignment/mainMethodAssignment/Funk.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mainMethodAssignment
+{
+    public class Funk // Allows funtion to be used on Program page
+    {
+        public double Mult(int numNum)  // Setting the function 
+        {
+            return numNum * 1000000;  // What we want the function to do is multiply numNum by a million
+        }
+        public int Mult(int numNum, decimal num1)
+        {
+            int returnFunk = Convert.ToInt32(num1);
+            return numNum * returnFunk;
+        }
+        public int Mult(string theFunk)
+        {
+            int returnFunk2 = Convert.ToInt32(theFunk);
+            return 42 * returnFunk2; 
+        }
+
+    }
+}
---
+++ myConsoleProject.cs/mainMethodAssignment/mainMethodAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mainMethodAssignment
+{
+    class Program
+    {        
+
+        static void Main(string[] args)
+        {
+            Funk mathOps = new Funk();
+            Console.WriteLine("Enter a number here: ");  // Message to user
+            var numNum = Convert.ToInt32(Console.ReadLine()); // User input
+            Console.WriteLine("Boost!"); // Write results
+            Console.WriteLine(mathOps.Mult(numNum));           
+            var funkyResult = mathOps.Mult(numNum);  // Calling the Mult function from Funk page               
+            Console.ReadLine();  // Display reesults to user
+            Console.WriteLine("Enter another number: ");
+            var num1 = Convert.ToDecimal(Console.ReadLine());
+            var funkyResult2 = mathOps.Mult(numNum, num1);            
+            Console.WriteLine(numNum + " times " + num1 + " equals " + funkyResult2);
+            Console.ReadLine();
+            Console.WriteLine("Pick another number: ");
+            var third = Console.ReadLine();
+            var funkyResult3 = mathOps.Mult(third);
+            Console.WriteLine(funkyResult3);
+            Console.ReadLine();
+        }
+    }
+}
