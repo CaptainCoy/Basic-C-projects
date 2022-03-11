55568536f69dac94596b4b6421383c1bed3e265c
Alex Coy <88282738+CaptainCoy@users.noreply.github.com>
2022-03-11 14:48:10 -0700

    Add files via upload

---
+++ myConsoleProject.cs/MethodSubmissionAssignment/MethodSubmissionAssignment/Moves.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class Moves
+    {
+        public int Add(int pTwo, int opt = 10)  // This sets var opt to 10 if the user decides to not put in a second number
+        {
+            return pTwo + opt;  // This adds the two variables together
+        }
+    }
+}
---
+++ myConsoleProject.cs/MethodSubmissionAssignment/MethodSubmissionAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Moves move = new Moves();  // This calls the Moves funtion to the Program
+            Console.WriteLine("Enter another number: ");  // Message to user
+            var pTwo = Convert.ToInt32(Console.ReadLine());  // User input
+
+            Console.WriteLine("Enter another if you'd like to: ");  // This asks for an optional second number to be entered
+            try
+            {
+                var opt = Convert.ToInt32(Console.ReadLine());  // This is the block of code that runs if the user decides to input a number
+                int endR = move.Add(pTwo, opt);  // This lets the Moves function know to use the two variables entered by the user
+                Console.WriteLine("The result is " + endR);
+                Console.ReadLine();
+            }
+            catch  // This block of code runs if the user decides not to input a second number
+            {
+                int endR = move.Add(pTwo);  // This lets the Moves funtion know to only use the first user input 
+                Console.WriteLine("The reult is " + endR);
+                Console.ReadLine();
+            }
+        }
+    }
+}
