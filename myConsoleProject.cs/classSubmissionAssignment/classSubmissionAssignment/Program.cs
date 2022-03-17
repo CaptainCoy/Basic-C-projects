e204c2fb2ad9f170c1dbe1c409f318da31f80e07
Alex Coy <88282738+CaptainCoy@users.noreply.github.com>
2022-03-17 12:36:43 -0600

    Add files via upload

---
+++ myConsoleProject.cs/classSubmissionAssignment/classSubmissionAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classSubmissionAssignment
+{
+    class Program
+    {
+        public static int answer { get; set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            whatItDo does = new whatItDo();  // this instantiates the function
+            Console.WriteLine("Enter a number to the void ");  // this prompts user to enter a number
+            int num = Convert.ToInt32(Console.ReadLine());  // user input
+            does.theThing(num); // this takes user input and applies it to the function             
+            Console.ReadLine();  // this allows the user to read the message that the function puts out
+
+            whatItDo doDo = new whatItDo();
+            Console.WriteLine("Give it another try... ");
+            Console.WriteLine("Enter another number ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter another number ");
+            int numNum = Convert.ToInt32(Console.ReadLine());
+            doDo.theThing(out int x);
+            Console.WriteLine("First number entered was: {0}", num1);
+            Console.WriteLine("Second number entered was: {0}", numNum);
+            Console.ReadLine();
+
+            Console.WriteLine("\n\n\nThanks for playing");  // This creates some space to make the exit message look nicer
+            whatItDo.Nombre(name);  // This calls the Nombre function
+            Console.ReadLine();  // This allows user to read the message
+            
+        }
+    }
+}
---
+++ myConsoleProject.cs/classSubmissionAssignment/classSubmissionAssignment/whatItDo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classSubmissionAssignment
+{
+    class whatItDo
+    {
+        public void theThing(int num)
+        {
+            int answer = num / 2;  // this takes user number and divides it by two
+            Console.WriteLine("I give you " + answer);  // message to user 
+        }
+
+        public void theThing(out int x)  // Overloading the method 
+        {
+            x = 10;
+            x *= x;
+
+        }
+
+        public static void Nombre(string name)  // This takes the string name and displays a message at the end
+        {
+            string aloha = "that was fun";  // This is part of the message
+            Console.WriteLine(name +"\n" + aloha);  // This displays string name and the message
+        }
+    }
+}
