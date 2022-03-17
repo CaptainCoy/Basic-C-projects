1baa534c8141a90a19ecfb088c37747e950e1a29
Alex Coy <88282738+CaptainCoy@users.noreply.github.com>
2022-03-16 18:24:02 -0600

    Add files via upload

---
+++ myConsoleProject.cs/classSubmissionAssignment/classSubmissionAssignment/whatItDo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classSubmissionAssignment
+{
+    class whatItDo
+    {
+        public void theThing(int num)
+        {
+            int answer = num / 2;  // this takes user number and divides it by two
+            Console.WriteLine("I give you " + answer);  // message to user 
+        }
+
+        public void TheThing(out int num1, out int numNum)  // Overloading the method 
+        {
+            Console.WriteLine("Enter another number ");
+            int numm1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter another number ");
+            int numNumm = Convert.ToInt32(Console.ReadLine());
+            int answer2 = numNumm * 1000 / numm1;
+            Console.WriteLine("Here's " + answer2);
+        }
+    }
+}
