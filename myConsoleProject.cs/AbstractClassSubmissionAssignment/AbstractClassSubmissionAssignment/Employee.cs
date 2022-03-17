b7a9c64e07fd10512750cdbf966461d1888958f8
Alex Coy <88282738+CaptainCoy@users.noreply.github.com>
2022-03-17 17:07:07 -0600

    Add files via upload

---
+++ myConsoleProject.cs/AbstractClassSubmissionAssignment/AbstractClassSubmissionAssignment/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassSubmissionAssignment
+{
+    public class Employee : Person  // Class employee can be instantiated and inherits attributes from class Person
+    {
+        public int ID { get; set; }  // Property specific to class Employee
+        
+        public new void SayName()  // Takes function inherited from class Person
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);  // Message to user
+        }
+    }
+}
