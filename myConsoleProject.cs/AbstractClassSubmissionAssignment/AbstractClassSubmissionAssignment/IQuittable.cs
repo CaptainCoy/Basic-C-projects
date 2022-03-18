be8f42e1c90e09ff213b385fad819262acfb0419
Alex Coy <88282738+CaptainCoy@users.noreply.github.com>
2022-03-18 12:17:10 -0600

    Add files via upload

---
+++ myConsoleProject.cs/AbstractClassSubmissionAssignment/AbstractClassSubmissionAssignment/IQuittable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassSubmissionAssignment
+{
+    public interface IQuittable
+    {
+        void Quit();  // This assigns the Quit function                 
+        
+    }
+}
