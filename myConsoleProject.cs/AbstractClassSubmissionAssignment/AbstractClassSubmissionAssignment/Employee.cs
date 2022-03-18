f11fb1cb913388ddcaa61a079254bca874008b4a
Alex Coy <88282738+CaptainCoy@users.noreply.github.com>
2022-03-17 18:27:53 -0600

    Add files via upload

--- myConsoleProject.cs/AbstractClassSubmissionAssignment/AbstractClassSubmissionAssignment/Employee.cs
+++ myConsoleProject.cs/AbstractClassSubmissionAssignment/AbstractClassSubmissionAssignment/Employee.cs
@@ -6,13 +6,17 @@ using System.Threading.Tasks;
 
 namespace AbstractClassSubmissionAssignment
 {
-    public class Employee : Person  // Class employee can be instantiated and inherits attributes from class Person
+    public class Employee : Person, IQuittable  // Class employee can be instantiated and inherits attributes from class Person and interface IQuittable
     {
         public int ID { get; set; }  // Property specific to class Employee
         
         public new void SayName()  // Takes function inherited from class Person
         {
             Console.WriteLine("Name: " + firstName + " " + lastName);  // Message to user
+        }        
+        public void Quit()  // This calls the Quit function from IQuittable
+        {
+            Console.WriteLine("I quit!");  // Quit message
         }
     }
 }
