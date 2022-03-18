e601d8a6b4caade715b65a7770892c3e806ae233
Alex Coy <88282738+CaptainCoy@users.noreply.github.com>
2022-03-18 11:03:38 -0600

    Add files via upload

--- myConsoleProject.cs/AbstractClassSubmissionAssignment/AbstractClassSubmissionAssignment/Employee.cs
+++ myConsoleProject.cs/AbstractClassSubmissionAssignment/AbstractClassSubmissionAssignment/Employee.cs
@@ -9,7 +9,22 @@ namespace AbstractClassSubmissionAssignment
     public class Employee : Person, IQuittable  // Class employee can be instantiated and inherits attributes from class Person and interface IQuittable
     {
         public int ID { get; set; }  // Property specific to class Employee
-        
+
+        public static bool operator == (Employee employee, Employee employee1)
+        {
+            if (employee.ID == employee1.ID)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator != (Employee employee, Employee employee1)
+        {
+            if (employee.ID != employee1.ID)
+                return false;
+            else
+                return true;
+        }
+
         public new void SayName()  // Takes function inherited from class Person
         {
             Console.WriteLine("Name: " + firstName + " " + lastName);  // Message to user
